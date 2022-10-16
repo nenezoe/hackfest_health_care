80f1d05819c53bb1dab14d4fa080ecc60c13f853
Juliet Okereke <okerekenene7@gmail.com>
2022-10-16 21:48:52 +0100

    Third commit

--- lib/screens/doctor_dashboard.dart
+++ lib/screens/doctor_dashboard.dart
@@ -8,16 +8,31 @@ class DoctorDashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Text('Dashboard'),
-
-
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text('Dashboard', style: TextStyle(fontSize: 18),),
+                ],
+              ),
+              SizedBox(height: 10,),
+              Text('Upcoming visit'),
+              SizedBox(height: 10,),
+              Center(
+                child: Container(
+                  height: 90,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
